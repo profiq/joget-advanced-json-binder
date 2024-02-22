75969a361bc81c67da6814679dd4cc4111f8e860
David Hrdlička <dhrdlicka@users.noreply.github.com>
2024-02-22 13:08:09 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it --rm -v ${PWD}:/root/.m2 -v ${PWD}:/usr/src/mymaven -w /usr/src/mymaven jogetworkflow/docker-maven-joget:8.0-SNAPSHOT mvn clean install
+docker run -i --rm -v ${PWD}:/root/.m2 -v ${PWD}:/usr/src/mymaven -w /usr/src/mymaven jogetworkflow/docker-maven-joget:8.0-SNAPSHOT mvn -B clean install
